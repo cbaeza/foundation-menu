4020f314f6d50fadc34adf914b7379455ddcf674
carlos.baeza <carlos.baeza@idealo.de>
2016-03-31 12:37:44 +0200

    add styles and remove event

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@
 			console.log('setEvent');
 			$(item).click( function(event){
 				console.log(event);
+				event.preventDefault();
+				event.stopPropagation();
 				var content = $(this).data('menu-content').replace(/\'/g, '"');
 				var o = JSON.parse(content);
 				c.showMenu(o, event);
@@ -37,9 +39,9 @@
 
 		Main.prototype.showMenu = function(content, event){
 			//console.log(content);
-			var menu = $('<div>').addClass('menu').append('<ul>');
+			var menu = $("<div>").addClass('menu').append("<ul class='menu-list'>");
 			for( var i=0 ; i < content.menu.length ; i++){
-				$(menu).append('<li>' + content.menu[i].item + '</li>');
+				$(menu).append("<li class='menu-list-item'><span class='" + content.menu[i].icon + "'><a href='#' class='menu-link' >" + content.menu[i].item + '</a></span></li>');
 			}
 			console.log('pageX: ' + event.pageX);
 			console.log('pageY: ' + event.pageY);
@@ -52,7 +54,16 @@
 				top: _top,
 				left: _left
 			});
-			menu.append('</ul>').appendTo('body');
+
+			$(menu).mouseleave(function(event){
+				event.preventDefault();
+				event.stopPropagation();
+				console.log('remove');
+				menu.remove();
+				menu.detach();
+			});
+
+			menu.append('</ul></div>').appendTo('body');
 		} // showMenu
 
 		
