7a021d7dce2b54a17d273caa92d0b54b57581515
cbaeza <carlos.baeza@posteo.de>
2016-04-12 22:30:46 +0200

    refactor

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,18 @@
 /**
-* Dynamic menu. Will be displayed for all elements that contain a data attribute
-* follow the following format:
-*	data-menu-content="{
-*		'menu':[
-*			{'item':'Vergleichen', 'icon':'icon-compare'   , 'link':'http://www.google.de', 'target':'_blank'},
-*			{'item':'Preiswecker', 'icon':'icon-pricealarm', 'link':'http://www.google.de', 'target':'_blank'},
-*			{'item':'Löschen'    , 'icon':'icon-delete'	   , 'link':'http://www.google.de', 'target':'_blank'}
-*		]}"> 
+ * Dynamic menu. Will be displayed for all elements that contain a data attribute
+ * follow the following format:
+ *	data-menu-content='{
+ *		"menu":[
+ *			{"item":"Vergleichen", "icon":"icon-compare", "action":"compare", "data":"111111"},
+ *			{"item":"Preiswecker", "icon":"icon-pricealarm", "action":"pricealarm", "data":"222222"},
+ *			{"item":"Löschen", "icon":"icon-delete", "action":"delete", "data":"333333"}
+ *		]}'>
  *
  * author: Carlos Baeza
  * version: 0.0.1
  * date: 31. March 2016
  */
-(function(){
+$(function(){
 	'use strict';
 
 	if(Modernizr.mobile){
@@ -40,13 +40,13 @@
 			}
 		}
 	}
-
-	var Menu = (function(){
+	
+	var Menu;
+	window.menu = Menu = (function(){
 
 		var options, viewPortHeigth, viewPortWidth;
 
 		function Menu(options){
-			console.log('Menu');
 			var c = this;
 			c.options = options;
 			c.init();
@@ -56,52 +56,51 @@
 			var c, $menues; 
 			c = this;
 			$menues = $('[data-menu-content]');
-			//console.log($menues.length);
 			if( typeof $menues !== 'undefined' && $menues.length > 0){
 				for(var i=0 ; i < $menues.length; i++){
-					c.setEvent($menues[i]);
+					c.bindEvent($menues[i]);
 				}	
 			}
 		} // init
 
-		Menu.prototype.setEvent = function(item){
+		Menu.prototype.bindEvent = function(item){
 			var c = this;
-			//console.log('setEvent');
 			$(item).click( function(event){
 				event.preventDefault();
 				event.stopPropagation();
-				var content = $(this).data('menu-content').replace(/\'/g, '"');
-				var o = JSON.parse(content);
-				c.showMenu(o, event);
+				var content = $(this).data('menu-content');
+				console.log(content)
+				c.showMenu(content, event);
 			});
 		} // setEvent
 
 		Menu.prototype.showMenu = function(content, event){
-			var c, menu, _top, _left, dialogWidth, viewPortWidth, viewPortHeigth;
+			var c, $menu, _top, _left, dialogWidth, viewPortWidth, viewPortHeigth;
 
 			c = this;
 			viewPortWidth = window.innerWidth || 320;
 			viewPortHeigth = window.innerHeight || 568;
 			dialogWidth = 152;
 			
-			console.log(event);
-
-			menu = $("<div class='menu'>").append("<ul class='menu-list'>");
+			$menu = $("<div class='menu'>").append("<ul class='menu-list'>");
 			for( var i=0 ; i < content.menu.length ; i++){
 				var item = content.menu[i];
-				$(menu).append("<li class='menu-list-item'><span class='" + item.icon + "'><a href='" + item.link + "' class='menu-link' target='" + item.target +"'>" + item.item + '</a></span></li>');
+				var $li = $("<li class='menu-list-item'>");
+				$li.append("<a href='#' class='menu-link " + item.icon + "'>" + item.item + "</a></li>");
+				$li.click(function(event){
+					c.handleMenuEvent(event);
+				});	
+				$($menu).append($li);
 			}
 
-
-
-			console.log('event.currentTarget.clientHeight: ' + event.currentTarget.clientHeight);
-			console.log('event.currentTarget.clientWidth: ' + event.currentTarget.clientWidth);
-			console.log('pageX: ' + event.pageX);
-			console.log('pageY: ' + event.pageY);
-			console.log('offsetX: ' + event.offsetX);
-			console.log('offsetY: ' + event.offsetY);
-			console.log('viewPortWidth: ' + viewPortWidth);
-			console.log('viewPortHeigth: ' + viewPortHeigth);
+			// console.log('event.currentTarget.clientHeight: ' + event.currentTarget.clientHeight);
+			// console.log('event.currentTarget.clientWidth: ' + event.currentTarget.clientWidth);
+			// console.log('pageX: ' + event.pageX);
+			// console.log('pageY: ' + event.pageY);
+			// console.log('offsetX: ' + event.offsetX);
+			// console.log('offsetY: ' + event.offsetY);
+			// console.log('viewPortWidth: ' + viewPortWidth);
+			// console.log('viewPortHeigth: ' + viewPortHeigth);
 			
 			_top = event.pageY + (event.currentTarget.clientHeight - event.offsetY) + 10 + 'px';
 
@@ -110,32 +109,27 @@
 			}else{
 				_left = event.pageX - event.offsetX + 'px';
 			}
-
-			console.log('top: ' + _top)
-			console.log('left: ' + _left)
-			
-			$(menu).css({
+			$($menu).css({
 				top: _top,
 				left: _left
 			});
 
-			/*$(menu).mouseleave(function(event){
-				event.preventDefault();
-				event.stopPropagation();
-				console.log('remove');
-				menu.remove();
-				menu.detach();
-			});*/
-			console.log(menu)
-
-			menu.append('</ul></div>').appendTo('body');
-			console.log($('.menu').width());
-			console.log($('.menu').css('width'));
+			$menu.append('</ul></div>').appendTo('body');
 		} // showMenu
+
+		Menu.prototype.handleMenuEvent = function(event){
+			console.log(event);
+			// TODO: do something
+
+			// then close menu
+			$(event.target).closest('.menu').remove();
+		} //handleMenuEvent
+
+
 		return Menu;
 	})(); //Menu
 	
 	var options = { someOption: 'some data to read' };
 	new Menu(options);
 
-})();
+});
